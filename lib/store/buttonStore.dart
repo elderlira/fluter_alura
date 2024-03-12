5cd351afe223a366448e70f9198c34c5d9e8ff65
elderlira <elderfranca@hotmail.com>
2024-03-11 22:14:26 -0300

    Created buttonStore

---
+++ lib/store/buttonStore.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ButtonStore extends ChangeNotifier {
+  // final List<ListView> _items = [];
+  var initialValue = false;
+
+  // List get buttonBalue => _items;
+
+  void changebuttonvalue() {
+    initialValue;
+  }
+}
